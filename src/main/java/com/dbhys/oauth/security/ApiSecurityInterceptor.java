d42f60e4340f7a32d86fe0931ebb56d7772f35d3
wangxudong <wangxudong@dbhys.com>
2025-01-12 09:00:39 +0800

    update java to 17, support springboot  3.x

--- src/main/java/com/dbhys/oauth/security/ApiSecurityInterceptor.java
+++ src/main/java/com/dbhys/oauth/security/ApiSecurityInterceptor.java
@@ -20,9 +20,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
